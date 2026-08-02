f532a392749f0c5d0fa3d694eb04a7b56532996b
chereshnya123 <nikos08012@gmail.com>
2026-08-02 18:11:55 +0300

    Refactor DB

--- db/migrations/001_сreate_questions.sql
+++ db/migrations/001_сreate_questions.sql
@@ -2,7 +2,9 @@ CREATE TABLE IF NOT EXISTS questions (
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
   tags TEXT,
-  vote_id INTEGER,
-  departments TEXT,
-  authors TEXT
+  votings_id TEXT, -- json list of votings ids
+  profile_committee_id INTEGER,
+  responsible_committee_id INTEGER,
+  other_committees TEXT, -- json list of committees ids
+  authors TEXT -- json list of authors ids
 );
--- db/migrations/005_create_votings.sql
+++ db/migrations/005_create_votings.sql
@@ -1,5 +1,6 @@
-CREATE TABLE IF NOT EXISTS votes (
+CREATE TABLE IF NOT EXISTS votings (
   id INTEGER PRIMARY KEY,
+  name TEXT,
   factions TEXT,
   result TEXT
 );
